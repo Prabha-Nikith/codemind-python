69ae8c7b0f0713799dd5fb253edc3f5145e88747
Prabha Nikith <119718378+Prabha-Nikith@users.noreply.github.com>
2023-08-26 15:55:24 +0530

    n=int(input()) s=0 if(n<0):     x=n-(2*n)     while(x!=0):         r=x%10         s=s*10+r         x=x//10     print(s-(2*s)) else:     while(n!=0):         r=n%10         s=s*10+r         n=n//10     print(s)

---
+++ Reverse_Integer.py
@@ -0,0 +1,15 @@
+n=int(input())
+s=0
+if(n<0):
+    x=n-(2*n)
+    while(x!=0):
+        r=x%10
+        s=s*10+r
+        x=x//10
+    print(s-(2*s))
+else:
+    while(n!=0):
+        r=n%10
+        s=s*10+r
+        n=n//10
+    print(s)
