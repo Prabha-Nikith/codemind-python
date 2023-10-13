1d43f016c951394041547049d534e7d34b7ab0a0
Prabha Nikith <119718378+Prabha-Nikith@users.noreply.github.com>
2023-10-13 14:57:44 +0530

    def oddNum(l1,n):     for i in  range(n-1,-1,-1):         if l1[i]%2==0:             return l1[i]     return 0 n=int(input()) l1=list(map(int,input().split())) print(oddNum(l1,n))

---
+++ Last_even_number_in_an_array.py
@@ -0,0 +1,8 @@
+def oddNum(l1,n):
+    for i in  range(n-1,-1,-1):
+        if l1[i]%2==0:
+            return l1[i]
+    return 0
+n=int(input())
+l1=list(map(int,input().split()))
+print(oddNum(l1,n))
